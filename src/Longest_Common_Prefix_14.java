85badd98090430a67a0cbcef9c3149ec947123ad
shado <shadowstakeus28@gmail.com>
2023-03-12 14:39:10 +0300

    Завершил успешно

--- src/Longest_Common_Prefix_14.java
+++ src/Longest_Common_Prefix_14.java
@@ -1,23 +1,32 @@
+import java.sql.SQLOutput;
 import java.util.Arrays;
 
 public class Longest_Common_Prefix_14 {
     public static void main(String[] args) {
-        System.out.println(longestCommonPrefix(new String[]{"a"}));//"flower","flow","flight"
+        System.out.println(longestCommonPrefix(new String[]{"flower","flower","flower","flower"}));//"flower","flow","flight"
     }
     public static String longestCommonPrefix(String[] strs) {
-        if(strs.length == 1) return strs[0];
+        if(strs.length == 1 && strs[0].length() == 1) return strs[0];
+        for (String s:
+                strs)
+           {
+               if(s.equals("")) return "";
+        }
         Arrays.sort(strs);
         StringBuilder sb = new StringBuilder();
-        int j = 0;
+        int sbLength = 0;
         for (int i = 0; i < strs.length; i++) {
-            while(j < strs[0].length() || j < strs[strs.length-1].length()) {
+            int j = 0;
+            while(j < strs[0].length() && j < strs[strs.length-1].length()) {
                 if(strs[0].charAt(j) == strs[strs.length-1].charAt(j)) {
                     sb.append(strs[0].charAt(j));
                     j++;
                 }
                 else return sb.toString();
             }
+            sbLength = j;
         }
-        return "";
+        sb.delete(sbLength, sb.length());
+        return sb.toString();
     }
 }
