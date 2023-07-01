721fc220e7918c42cf7fcd6ff01d925899f857f8
shado <shadowstakeus28@gmail.com>
2023-07-01 21:14:29 +0300

    268

---
+++ .metadata/.plugins/org.eclipse.core.resources/.projects/Scratch/org.sonarlint.eclipse.core/issues/index.pb
@@ -0,0 +1,5 @@
+
+A
+src/com/Test.java,1\e\1e3e989e424b7a194323ee0005cd943dec795e97
+K
+src/com/RecursionCheck.java,4\d\4d8655828997ac736d28c570c88d60ac62297573
--- src/Leet_Code_Problems/Kotlin/ContainsDuplicate217.kt
+++ src/Leet_Code_Problems/Kotlin/ContainsDuplicate217.kt
@@ -1,18 +1,9 @@
 package Leet_Code_Problems.Kotlin
 
 fun main(args: Array<String>){
-    containsDuplicate(intArrayOf(1,2,3,4));
-
-    var arr: IntArray = intArrayOf(123,23,2,2);
-    var arr2: IntArray;
-
-
+    print(containsDuplicate(intArrayOf(1,2,3,4)))
 }
 
-fun containsDuplicate(nums: IntArray?): Boolean {
-    val set: MutableSet<Int> = HashSet()
-    for (i in nums.indices) {
-
-    }
-    return false;
+fun containsDuplicate(nums: IntArray): Boolean {
+    return nums.size != nums.toSet().size
 }
