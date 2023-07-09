329d743ee196ae29c0ecbc550e2531e5427203a4
shado <shadowstakeus28@gmail.com>
2023-07-09 17:28:22 +0300

    delete: redundant duplicate package.

--- src/Leet_Code_Problems/Kotlin/ContainsDuplicate217.kt
+++
@@ -1,9 +0,0 @@
-package Leet_Code_Problems.Kotlin
-
-fun main(args: Array<String>){
-    print(containsDuplicate(intArrayOf(1,2,3,4)))
-}
-
-fun containsDuplicate(nums: IntArray): Boolean {
-    return nums.size != nums.toSet().size
-}
