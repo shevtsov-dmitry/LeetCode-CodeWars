25d26d283d5a2cda9d353cf2dc4cb2628af8a2ec
shado <shadowstakeus28@gmail.com>
2023-08-25 16:32:58 +0300

    Solved MaximumDepthofBinaryTree104.java.
    Strange thing that was a bug with answer in leetcode, but in my compiler everything was OK.

--- src/leet_code/Java/BinaryTreeInorderTraversal94.java
+++ src/leet_code/Java/BinaryTreeInorderTraversal94.java
@@ -5,27 +5,27 @@ import java.util.List;
 
 public class BinaryTreeInorderTraversal94 {
     public static void main(String[] args) {
-//        TreeNode node = new TreeNode(1);
-//        node.right = new TreeNode(2);
-//        node.right.left = new TreeNode(3);
-        TreeNode node = new TreeNode(1);
-        node.right = new TreeNode(3);
-        node.right.right = new TreeNode(6);
-        node.left = new TreeNode(2);
-        node.left.left = new TreeNode(4);
-        node.left.right = new TreeNode(5);
+//        Node node = new Node(1);
+//        node.right = new Node(2);
+//        node.right.left = new Node(3);
+        Node node = new Node(1);
+        node.right = new Node(3);
+        node.right.right = new Node(6);
+        node.left = new Node(2);
+        node.left.left = new Node(4);
+        node.left.right = new Node(5);
         var list = inorderTraversal(node);
         System.out.println("list = " + list);
     }
 
-    private static List<Integer> inorderTraversal(TreeNode node) {
+    private static List<Integer> inorderTraversal(Node node) {
         List<Integer> result = new ArrayList<>();
         traverse(node, result);
         return result;
     }
 
     //    Output: [1,3,2]
-    private static void traverse(TreeNode node, List<Integer> list) {
+    private static void traverse(Node node, List<Integer> list) {
 //        if(node.left == null && node.right == null) return;
         if(node == null) return;
         traverse(node.left, list);
@@ -34,15 +34,15 @@ public class BinaryTreeInorderTraversal94 {
     }
 
 
-    static class TreeNode {
-        TreeNode right;
-        TreeNode left;
+    static class Node {
+        Node right;
+        Node left;
         int val;
 
-        public TreeNode() {
+        public Node() {
         }
 
-        public TreeNode(int val) {
+        public Node(int val) {
             this.val = val;
         }
     }
--- src/leet_code/Java/ReorganizeString767.java
+++ src/leet_code/Java/ReorganizeString767.java
@@ -1,6 +1,7 @@
 package leet_code.Java;
 
 public class ReorganizeString767 {
+
     public static void main(String[] args) {
         String s = "aabaacc";
 //        String s = "aaf";
@@ -51,78 +52,3 @@ public class ReorganizeString767 {
     }
 }
 // SOLUTION
-
-
-/*
-    public static String reorganizeString(String S) {
-        int[] hash = new int[26]; // english alphabet hash
-        for (int i = 0; i < S.length(); i++) {
-            hash[S.charAt(i) - 'a']++; // count letters
-        }
-
-        int max = 0, letter = 0;
-        for (int i = 0; i < hash.length; i++) {
-            if (hash[i] > max) {
-                max = hash[i];
-                letter = i;
-            }
-        }
-
-        // if amount of maximal occurred letter in string is higher
-        // than a half of string length, then it is not possible
-        // to not repeat letter twice in a row.
-        // And we need length +1 in case if length is odd.
-        if (max > (S.length() + 1) / 2) {
-            return "";
-        }
-
-        char[] res = new char[S.length()];
-        int idx = 0;
-        while (hash[letter] > 0) {
-            res[idx] = (char) (letter + 'a');
-            idx += 2;
-            hash[letter]--;
-        }
-        for (int i = 0; i < hash.length; i++) {
-            while (hash[i] > 0) {
-                if (idx >= res.length) {
-                    idx = 1;
-                }
-                res[idx] = (char) (i + 'a');
-                idx += 2;
-                hash[i]--;
-            }
-        }
-        return String.valueOf(res);
-    }
-}
-*/
-
-/*  Nice solution! This is how I understand it:
-
-  We construct the resulting string in sequence: at position 0, 2, 4, ... and then 1, 3, 5, ...
-  In this way, we can make sure there is always a gap between the same characters
-
-  Consider this example: "aaabbbcdd", we will construct the string in this way:
-
-  a _ a _ a _ _ _ _ // fill in "a" at position 0, 2, 4
-  a b a _ a _ b _ b // fill in "b" at position 6, 8, 1
-  a b a c a _ b _ b // fill in "c" at position 3
-  a b a c a d b d b // fill in "d" at position 5, 7
-  */
-
-/*    Great solution has illustrated how this solution works.
-    Let me give more explanation.
-
-            Firstly, the most-frequent char count > (S.size() + 1) / 2 then for sure we have to fail it.
-    But why any strings with most-frequent char <= (S.size() + 1) / 2 MUST work?
-
-    Take an example: for a length 7 string, suppose there are 4 x, and 3 other chars, then:
-    x_x_x_x
-    No matter what are other 3 chars are, it must work.
-
-            if there are 3 x in this length 7 string, and the second-most-frequent char is y, then we know count of y has to be less than or equal to 3, and:
-    x_x_x_y
-    Here if we want to fail the string, we need to fill all empty slots with y, to make the last two slots are same.
-            But this means count(y) = 4, so => contradiction.*/
-
