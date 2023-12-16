88c89a3c29759f31f404f385fec1948586903c7a
shevtsov-dmitry <shadowstakeus28@gmail.com>
2023-12-16 21:05:40 +0300

    good try for learning postfix and prefix algo

--- src/leet_code/java/ProductOfArrayExceptSelf238.java
+++ src/leet_code/java/ProductOfArrayExceptSelf238.java
@@ -3,63 +3,42 @@ package leet_code.java;
 import java.util.Arrays;
 
 public class ProductOfArrayExceptSelf238 {
-    public static int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-
-        // Create prefix and postfix arrays to store products
-        int[] prefix = new int[n];
-        int[] postfix = new int[n];
-
-        // Initialize prefix array
-        prefix[0] = 1;
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1] * nums[i - 1];
-        }
-
-        // Initialize postfix array
-        postfix[n - 1] = 1;
-        for (int i = n - 2; i >= 0; i--) {
-            postfix[i] = postfix[i + 1] * nums[i + 1];
-        }
-
-        // Calculate the final result array
-        int[] result = new int[n];
-        for (int i = 0; i < n; i++) {
-            result[i] = prefix[i] * postfix[i];
-        }
-
-        return result;
-    }
 
     public static void main(String[] args) {
+        int[] ints = new int[]{1, 2, 3, 4};
+        productExceptSelf(ints);
 
-        int[] nums = {1, 2, 3, 4};
-        int[] result = productExceptSelf(nums);
-
-        // Print the result
-        System.out.print("Output: [");
-        for (int i = 0; i < result.length; i++) {
-            System.out.print(result[i]);
-            if (i < result.length - 1) {
-                System.out.print(", ");
-            }
-        }
-        System.out.println("]");
     }
-//    public static void main(String[] args) {
-////      Input: nums = [1,2,3,4]
-////      Output: [24,12,8,6]
-//        int[] productExceptSelf = productExceptSelf(new int[]{1, 2, 3, 4});
-//        System.out.println("productExceptSelf = " + Arrays.toString(productExceptSelf));
-//    }
-//
-//    public static int[] productExceptSelf(int[] nums) {
-//        int len = nums.length;
-//        int[] answer = new int[len];
-//        for (int i = 0; i < len - 1; i++) {
-//            answer[i] = nums[i + 1] * nums[i];
-//        }
-//
-//        return answer;
-//    }
+
+    public static int[] productExceptSelf(int[] nums) {
+        int n= nums.length;
+        int[] util = new int[n];
+        util[n - 1] = 1;
+
+        int tempo = nums[n - 1];
+        fillUtilArray(nums, tempo, util);
+        tempo = nums[0];
+        fillNumsArray(nums, tempo);
+
+
+
+        System.out.println(STR."nums = \{Arrays.toString(nums)}");
+        System.out.println(STR."util = \{Arrays.toString(util)}");
+        return nums;
+    }
+
+    public static void fillNumsArray(int[] nums, int tempo) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            tempo = tempo * nums[i + 1];
+            nums[i + 1] = tempo;
+        }
+    }
+
+    public static void fillUtilArray(int[] nums, int tempo, int[] arr) {
+        for (int i = nums.length - 2; i >= 0; i--) {
+            tempo = tempo * nums[i];
+            arr[i] = tempo;
+        }
+    }
+
 }
