b4760ce611971990a29e8a3a9948dfc7369a32ca
shevtsov-dmitry <shadowstakeus28@gmail.com>
2023-12-17 16:32:11 +0300

    leetcode:

--- src/leet_code/java/ProductOfArrayExceptSelf238.java
+++ src/leet_code/java/ProductOfArrayExceptSelf238.java
@@ -5,40 +5,59 @@ import java.util.Arrays;
 public class ProductOfArrayExceptSelf238 {
 
     public static void main(String[] args) {
-        int[] ints = new int[]{1, 2, 3, 4};
+        int[] ints = new int[] { 1, 2, 3, 4 };
         productExceptSelf(ints);
 
     }
 
     public static int[] productExceptSelf(int[] nums) {
-        int n= nums.length;
-        int[] util = new int[n];
-        util[n - 1] = 1;
+        int n = nums.length;
+        int[] prefix = new int[n];
+        int[] postfix = new int[n];
 
-        int tempo = nums[n - 1];
-        fillUtilArray(nums, tempo, util);
-        tempo = nums[0];
-        fillNumsArray(nums, tempo);
+        int tempo = nums[0];
+        fillPrefix(nums, n, prefix, tempo);
 
+        tempo = nums[n - 1];
+        fillPostfix(nums, n, postfix, tempo);
 
+        calculateResult(nums, n, prefix, postfix);
 
-        System.out.println(STR."nums = \{Arrays.toString(nums)}");
-        System.out.println(STR."util = \{Arrays.toString(util)}");
+        // log(nums, prefix, postfix);
         return nums;
     }
 
-    public static void fillNumsArray(int[] nums, int tempo) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            tempo = tempo * nums[i + 1];
-            nums[i + 1] = tempo;
+    static void calculateResult(int[] nums, int n, int[] prefix, int[] postfix) {
+        for (int i = 0; i < n; i++) {
+            if (i == 0)
+                nums[i] = postfix[1];
+            else if (i == n - 1)
+                nums[i] = prefix[n - 2];
+            else
+                nums[i] = prefix[i - 1] * postfix[i + 1];
         }
     }
 
-    public static void fillUtilArray(int[] nums, int tempo, int[] arr) {
-        for (int i = nums.length - 2; i >= 0; i--) {
+    static void fillPostfix(int[] nums, int n, int[] postfix, int tempo) {
+        postfix[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
             tempo = tempo * nums[i];
-            arr[i] = tempo;
+            postfix[i] = tempo;
         }
     }
+
+    static void fillPrefix(int[] nums, int n, int[] prefix, int tempo) {
+        prefix[0] = nums[0];
+        for (int i = 0; i < n - 1; i++) {
+            tempo = tempo * nums[i + 1];
+            prefix[i + 1] = tempo;
+        }
+    }
+
+    // static void log(int[] nums, int[] prefix, int[] postfix) {
+    //     System.out.println("prefix: " + Arrays.toString(prefix));
+    //     System.out.println("postfix: " + Arrays.toString(postfix));
+    //     System.out.println("nums: " + Arrays.toString(nums));
+    // }
 
 }
