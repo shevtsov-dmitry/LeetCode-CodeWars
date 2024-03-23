e7e2801af9335de5bc306438dafcfe6df48687a4
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-03-23 12:51:55 +0300

    leetcode: optimal solution for two sum sorted

--- src/leet_code/TwoSumII_InputArrayIsSorted167.java
+++ src/leet_code/TwoSumII_InputArrayIsSorted167.java
@@ -8,22 +8,15 @@ void main() {
     int[] ints = twoSum(new int[]{2, 7, 11, 15}, 9);
     System.out.println(Arrays.toString(ints));
 }
-
 public int[] twoSum(int[] numbers, int target) {
-    Map<Integer, Integer> map = new HashMap<>();
-    for (int i = 0, j = numbers.length - 1; i < numbers.length; i++, j--) {
-        if (map.containsKey(target - numbers[i])) {
-            int[] ints = {map.get(target - numbers[i]) + 1, i + 1};
-            Arrays.sort(ints);
-            return ints;
-        } else if (map.containsKey(target - numbers[j])) {
-            int[] ints = {map.get(target - numbers[j]) + 1, j + 1};
-            Arrays.sort(ints);
-            return ints;
+    int i = 0, j = numbers.length - 1;
+    while (i <= j) {
+        if (numbers[j] + numbers[i] == target) {
+            return new int[]{i + 1,j + 1};
         }
-        map.put(numbers[i], i);
-        map.put(numbers[j], j);
-
+        if (numbers[i] + numbers[j] > target) j--;
+        else i++;
     }
+
     return new int[]{};
 }
