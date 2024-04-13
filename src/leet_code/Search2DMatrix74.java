b54644b13679ac4f869d7ae1c5282ac848211aeb
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-04-13 19:05:59 +0300

    leetcode: Search2DMatrix74.java

--- src/leet_code/Search2DMatrix74.java
+++ src/leet_code/Search2DMatrix74.java
@@ -1,8 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 void main() {
     int[][] matrix = {
             {1, 3, 5, 7},
@@ -13,26 +8,27 @@ void main() {
     System.out.println(b);
 }
 
-    public boolean searchMatrix(int[][] matrix, int target) {
-    int[] array = new int[0];
+public boolean searchMatrix(int[][] matrix, int target) {
+    int arrayToBSIndex = 0;
+    if (matrix.length != 0) {
         for (int i = 0; i < matrix.length - 1; i++) {
-            if(target >= matrix[i][0] && target <= matrix[i + 1][0]) {
-                array = matrix[i];
+            if (matrix[i][0] <= target && matrix[i + 1][0] >= target) {
+                arrayToBSIndex = i;
             }
         }
-
-        int l = 0, r = array.length - 1, mid = -1;
-        while (l<=r) {
-            mid = l + (r-l) / 2;
-            if(array[mid] == target) {
-                return true;
-            } else if (array[mid] < target) {
-                l = mid + 1;
-            }
-            else {
-                r = mid - 1;
-            }
-        }
-
-        return false;
     }
+    return bs(matrix[arrayToBSIndex], target);
+}
+
+private boolean bs(int[] nums, int target) {
+    int l = 0, r = nums.length - 1, mid;
+    while (l <= r) {
+        mid = l + (r - l) / 2;
+        if (nums[mid] == target) {
+            return true;
+        } else if (nums[mid] < target) {
+            l = mid + 1;
+        } else r = mid - 1;
+    }
+    return false;
+}
