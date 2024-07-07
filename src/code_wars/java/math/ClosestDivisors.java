1cec3949603ba813f56b9f3a0e8fbaac77aa4635
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-07-07 11:26:04 +0300

    ClosestDivisors.java

---
+++ src/code_wars/java/math/ClosestDivisors.java
@@ -0,0 +1,28 @@
+package code_wars.java.math;
+
+import java.util.Arrays;
+
+/**
+ * ClosestDivisors
+ */
+public class ClosestDivisors {
+
+    public static void main(String[] args) {
+        assert Arrays.equals(closestDivisors(8), new int[] { 3, 3 }) : 1;
+        assert Arrays.equals(closestDivisors(123), new int[] { 5, 25 }) : 2;
+        assert Arrays.equals(closestDivisors(999), new int[] { 25, 40 }) : 3;
+    }
+
+    public static int[] closestDivisors(int num) {
+        for (int i = (int) Math.sqrt(num + 2); i > 0; i--) {
+            if ((num + 1) % i == 0) {
+                return new int[] { i, (num + 1) / i };
+            }
+            if ((num + 2) % i == 0) {
+                return new int[] { i, (num + 2) / i };
+            }
+        }
+        return new int[] {};
+    }
+
+}
